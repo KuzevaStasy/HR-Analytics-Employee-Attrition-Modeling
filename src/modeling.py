628ce9af38a3538885516a3b46a96fe83405c13a
Stanislava Kuzeva <kuzevastasy@gmail.com>
2026-08-12 15:06:40 +0300

    Update modeling.py

--- src/modeling.py
+++ src/modeling.py
@@ -1,25 +1,9 @@
 import pandas as pd
 
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import roc_auc_score
-
-
-def split_data(df: pd.DataFrame, target: str = "Attrition"):
-    """
-    Split dataset into train and test sets.
-    """
-    X = df.drop(columns=target)
-    y = df[target]
-
-    return train_test_split(
-        X, y,
-        test_size=0.25,
-        random_state=42,
-        stratify=y
-    )
 
 
 def train_logistic_regression(X_train, y_train):
